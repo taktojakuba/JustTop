c4c8e5f70eb00d6cb95e12f959b735259d8a1faf
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-08-23 23:56:41 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -2,19 +2,33 @@
 #include <string>
 #include <vector>
 
+// ftxui
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
 // include modules 
 #include "modules/headers/example.h"
 
 using namespace std;
+using namespace ftxui;
 
 int main(int argc, char* argv[])
 {
   vector<string> modules = {
     "example: " + example(),
   };
-  for(int i = 0;i<modules.size();i++){
-    cout << modules[i] << endl;
-  };
-  
-      return 0;
+
+  // build lines out of module strings
+  Elements lines;
+  for(size_t i = 0;i<modules.size();i++){
+    lines.push_back(text(modules[i]));
+  }
+
+  // static one-shot render
+  auto document = vbox(lines) | border;
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+  cout << screen.ToString() << endl;
+
+  return 0;
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: jtop
 
 jtop: main.cpp modules/*.cpp
-	g++ -std=c++17 -O2 -march=native -flto main.cpp modules/*.cpp -o jtop
+	g++ -std=c++17 -O2 -march=native -flto main.cpp modules/*.cpp -o jtop -lftxui-component -lftxui-dom -lftxui-screen
 install: jtop
 	cp jtop /usr/local/bin/jtop
 
